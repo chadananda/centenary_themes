f0b5aaac9d6da24a42fd7efc6313d78cc24effe5
SCC Dev Team <chadnanada@gmail.com>
2011-09-14 20:59:09 +0000

    adding timelinemap block

--- centenary/template.php
+++ centenary/template.php
@@ -63,13 +63,13 @@
 
 /**
  * Implementation of HOOK_theme().
- *
+ */
 function centenary_theme(&$existing, $type, $theme, $path) {
   $hooks = zen_theme($existing, $type, $theme, $path);
   // Add your theme hooks like this:
   /*
   $hooks['hook_name_here'] = array( // Details go here );
-  *
+  */
   // @TODO: Needs detailed comments. Patches welcome!
   return $hooks;
 }
